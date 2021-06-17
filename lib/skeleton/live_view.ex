b0030d7283b2e2f7123fa63132421fbfff8a40a8
Diego Nogueira <diegoalvareznogueira@gmail.com>
2021-06-17 17:29:02 +0000

    liveview fixed

--- lib/skeleton/live_view.ex
+++ lib/skeleton/live_view.ex
@@ -2,6 +2,7 @@ defmodule Skeleton.Phoenix.LiveView do
   # Callbacks
 
   @callback is_authenticated(Plug.Socket.t) :: Boolean.t
+  @callback fallback(Plug.Conn.t()) :: Plug.Conn.t()
 
   defmacro __using__(opts) do
     alias Skeleton.Phoenix.LiveView
@@ -11,17 +12,17 @@ defmodule Skeleton.Phoenix.LiveView do
 
       # Ensure authenticated
 
-      def ensure_authenticated({:error, error}), do: {:error, error}
+      def ensure_authenticated({:error, socket, error}), do: {:error, socket, error}
       def ensure_authenticated(socket), do: LiveView.do_ensure_authenticated(@live_view, socket)
 
       # Ensure not authenticated
 
-      def ensure_not_authenticated({:error, error}), do: {:error, error}
+      def ensure_not_authenticated({:error, socket, error}), do: {:error, socket, error}
       def ensure_not_authenticated(socket), do: LiveView.do_ensure_not_authenticated(@live_view, socket)
 
       # Resolve
 
-      def resolve({:error, error}, _), do: {:error, error}
+      def resolve({:error, socket, error}, _), do: @live_view.fallback(socket, error)
       def resolve(socket, callback), do: callback.(socket)
     end
   end
@@ -48,8 +49,8 @@ defmodule Skeleton.Phoenix.LiveView do
 
   # Return unauthorized
 
-  def unauthorized(_socket) do
-    {:error, :unauthorized}
+  def unauthorized(socket) do
+    {:error, socket, :unauthorized}
   end
 
   # Retorn success
