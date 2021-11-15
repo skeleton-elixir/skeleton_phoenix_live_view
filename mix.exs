85879537127f88fc601a4aee2d407d2f445373a0
Diego Nogueira <diegoalvareznogueira@gmail.com>
2021-11-15 11:22:17 -0300

    Updated deps. Added credo

--- mix.exs
+++ mix.exs
@@ -20,7 +20,14 @@ defmodule SkeletonPhoenixLiveView.MixProject do
       description: description(),
       maintainers: @maintainers,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -34,7 +41,9 @@ defmodule SkeletonPhoenixLiveView.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
     ]
   end
 
