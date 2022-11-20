35da7d6a32efac8e0b0c5128126b5b3ed20cf7d3
Diego Nogueira <diegoalvareznogueira@gmail.com>
2022-11-20 11:53:53 -0300

    Updated phoenix live view to 0.18.3

--- mix.exs
+++ mix.exs
@@ -41,11 +41,11 @@ defmodule SkeletonPhoenixLiveView.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:phoenix_live_view, "~> 0.17.5"},
-      {:floki, ">= 0.33.0", only: :test},
+      {:phoenix_live_view, "~> 0.18.3"},
+      {:floki, ">= 0.34.0", only: :test},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 1.6.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.15", only: :test}
     ]
   end
 
